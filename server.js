45af5dfb456306b810c13e85306dce8448191605
sstephensMCSE <sstephens.mcse@gmail.com>
2021-02-02 10:51:00 -0500

    edit mongoose connection logic

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to Mongo database via Mongoose
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/AtlasMongoDB", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
